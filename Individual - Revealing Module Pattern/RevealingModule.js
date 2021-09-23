66197adfd2e8fa3d5e98fb96f78811604615a678
Alex Souza <spfc.alex4@gmail.com>
2021-09-23 19:44:56 -0300

    Update RevealingModule.js

--- Individual - Revealing Module Pattern/RevealingModule.js
+++ Individual - Revealing Module Pattern/RevealingModule.js
@@ -2,10 +2,10 @@ const francisco = (function (){
     const name = "Francisco Paulino";
     const address = "Cachoeira dos Índios";
     const introduce = function (){
-        return 'My name is ${name}';
+        return `My name is ${name}`;
     };
     const report = function (){
-        return 'I live in ${address}';
+        return `I live in ${address}`;
     };
     //'return' retorna publicamente os ponteiros das funções
     //e propriedades privadas
